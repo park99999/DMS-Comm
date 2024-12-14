678b024eaeff1da86bb0bcd317b9eee1ca7a2db5
park99999 <111122515+park99999@users.noreply.github.com>
2024-12-14 20:50:26 +0900

    Update MemberService.java

--- comm/bulletin/src/main/java/dms/service/MemberService.java
+++ comm/bulletin/src/main/java/dms/service/MemberService.java
@@ -3,6 +3,9 @@ package dms.service;
 import dms.domain.Member;
 import dms.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +14,14 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
+    private ApplicationContext context;
+
     public Member createMember(Member member) {
+        if(Objects.equals(member.getMemberName(), "exit")){
+            //서버 종료
+            SpringApplication.exit(context, () -> 0);
+        }
         return memberRepository.save(member);
     }
 
